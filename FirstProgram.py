2de1b5ec3620ab74e4c3ac13700bc1d640c5b680
dillanklug <klugdillan@gmail.com>
2024-08-28 16:40:31 +0000

    First Update

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,22 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Dillan Klug
+#Date:8/28/2024
+#Assignment:Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello user")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("How old are you? ")
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
+  birthY = 2024 - int(age)
+  print("That means you were born around", birthY)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,21 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name:Dillan Klug
+#Date:8/28/2024
+#Assignment:Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  onomatopoeia = input("Onomatopoeia (Sound) - ")
+  verb = input("Past Tense Verb (Action) - ")
+  adjective = input("Adjective (Description) - ")
+  animal = input ("Favorite Animal - ")
+  emotion = input ("Emotion - ")
+  number = input ("Number - ")
   #Print the story with the user supplied words.
-
+  print("Here is your Madlib!")
+  print()
+  print(""", onomatopoeia + """ + "!,", "he said as he", verb, "away from the", adjective, animal + ". He had never felt so", emotion, "in his", number, "years of life!" )
 
 
 #Call the main function if this is the file being run.
